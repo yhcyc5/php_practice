69208b6e059d8bb1cccfba59279f115754d48f12
Zoe Chen <Zoe.chen@inforise.com.tw>
2024-02-21 11:57:41 +0800

    composer

---
+++ form.php
@@ -0,0 +1,25 @@
+<?php
+if (isset($_POST['action']) && $_POST['action'] == 'submitted') {
+    echo '<pre>';
+
+    print_r($_POST);
+    echo '<a href="'. $_SERVER['PHP_SELF'] .'">Please try again</a>';
+
+    echo '</pre>';
+} else {
+?>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    Name:  <input type="text" name="personal[name]"><br />
+    Email: <input type="text" name="personal[email]"><br />
+    Beer: <br>
+    <select multiple name="beer[]">
+        <option value="warthog">Warthog</option>
+        <option value="guinness">Guinness</option>
+        <option value="stuttgarter">Stuttgarter Schwabenbr</option>
+    </select><br />
+    <input type="hidden" name="action" value="submitted" />
+    <input type="submit" name="submit" value="submit me!" />
+</form>
+<?php
+}
+?>
